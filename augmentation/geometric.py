04bf474fa99a3a735f0b66430ce8b27b6f130a82
adrianB3 <balanescuadrian71@gmail.com>
2020-10-25 19:57:29 +0200

    added geometrical transforms

--- augmentation/geometric.py
+++ augmentation/geometric.py
@@ -4,6 +4,42 @@ import numpy as np
 from augmentation.data_types import Augmentation, Data
 
 
+class Translation(Augmentation):
+    def __init__(self, params):
+        self.params = params
+
+    def process(self, data: Data):
+        (h, w) = data.data['image'].shape[:2]
+
+        T = np.float32([[1, 0, self.params['t_y']], [0, 1, self.params['t_x']]])
+
+        data.data['image'] = cv2.warpAffine(data.data['image'], T, (w, h))
+
+
+class Scaling(Augmentation):
+    def __init__(self, params):
+        self.params = params
+
+    def process(self, data: Data):
+        (h, w) = data.data['image'].shape[:2]
+
+        S = np.float32([[self.params['s_x'], 0, 0], [0, self.params['s_y'], 0]])
+
+        data.data['image'] = cv2.warpAffine(data.data['image'], S, (w, h))
+
+
+class Shear(Augmentation):
+    def __init__(self, params):
+        self.params = params
+
+    def process(self, data: Data):
+        (h, w) = data.data['image'].shape[:2]
+
+        SH = np.float32([[1, self.params['sh_x'], 0], [self.params['sh_y'], 1, 0]])
+
+        data.data['image'] = cv2.warpAffine(data.data['image'], SH, (w, h))
+
+
 class Rotation(Augmentation):
     def __init__(self, params):
         self.params = params
