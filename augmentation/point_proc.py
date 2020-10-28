f5ed67a4e36754ad8f59962ba3e3f4e8e0b6c564
adrianB3 <balanescuadrian71@gmail.com>
2020-10-28 18:10:54 +0200

    More noise types

--- augmentation/point_proc.py
+++ augmentation/point_proc.py
@@ -5,26 +5,26 @@ import numpy as np
 import cv2
 
 
-class Bias(Augmentation):
+class Luminosity(Augmentation):
     def __init__(self, params):
         self.params = params
 
     def process(self, data: Data):
         img = data.data['image'].copy()
         img = img.astype('float32')
-        img += self.params['b']
+        img += self.params['bias']
         data.data['image'] = img.astype('uint8')
 
 
-class Gain(Augmentation):
+class Contrast(Augmentation):
     def __init__(self, params):
         self.params = params
 
     def process(self, data: Data):
         img = data.data['image'].copy()
         img = img.astype('float32')
-        if self.params['a'] != 0:
-            img *= self.params['a']
+        if self.params['gain'] != 0:
+            img *= self.params['gain']
 
         data.data['image'] = img.astype('uint8')
 
@@ -39,26 +39,56 @@ class GammaCorrection(Augmentation):
         inv_gamma = 1.0 / self.params['gamma']
         table = np.array([((i / 255.0) ** inv_gamma) * 255 for i in np.arange(0, 256)])
         corrected_image = cv2.LUT(img.astype(np.uint8), table.astype(np.uint8))
-        # corrected_image = np.power(img, self.params['gamma'])
 
         data.data['image'] = corrected_image
 
 
 class Noise(Augmentation):
+    def __init__(self, params):
+        self.params = params
 
     def process(self, data: Data):
         img = data.data['image']
-        noise = np.random.randint(0, 50, (img.shape[0], img.shape[1]))
-        jitter = np.zeros_like(img)
-        if img.shape[2] == 1:
-            jitter[:, :, 1] = noise
-        else:
-            jitter[:, :, 0] = noise
-            jitter[:, :, 1] = noise
-            jitter[:, :, 2] = noise
+        if img.ndim == 3:
+            h, w, c = img.shape
+        elif img.ndim == 2:
+            h, w = img.shape
+        if self.params['type'] == "gaussian":
+            mean = self.params['gaussian']['mean']
+            var = self.params['gaussian']['var']
+            sigma = var ** 0.5
+            gauss = np.random.normal(mean, sigma, (h, w, c))
+            gauss = gauss.reshape(h, w, c)
+            noisy = img + gauss
+            data.data['image'] = noisy
 
-        noise_added = cv2.add(img, jitter)
-        data.data['image'] = noise_added
+        if self.params['type'] == "sp":
+            svsp = 0.5
+            amount = self.params['sp']['amount']
+            # Salt mode
+            num_salt = np.ceil(amount * img.size * svsp)
+            coords = [np.random.randint(0, i - 1, int(num_salt))
+                      for i in img.shape]
+            img[coords] = 1
+
+            # Pepper mode
+            num_pepper = np.ceil(amount * img.size * (1. - svsp))
+            coords = [np.random.randint(0, i - 1, int(num_pepper))
+                      for i in img.shape]
+            img[coords] = 0
+            data.data['image'] = img
+
+        if self.params['type'] == "poisson":
+            vals = len(np.unique(img))
+            vals = 2 ** np.ceil(np.log2(vals))
+            noisy = np.random.poisson(img * vals) / float(vals)
+            data.data['image'] = noisy
+
+        if self.params['type'] == "speckle":
+            gauss = np.random.randn(h, w, c)
+            gauss = gauss.reshape(h, w, c)
+            noisy = img + img * gauss
+            data.data['image'] = noisy
 
 
 class HistEq(Augmentation):
