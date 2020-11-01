9a8f815228ca9e2e82cb17fd96b04b0df223999b
adrianB3 <balanescuadrian71@gmail.com>
2020-11-02 00:02:41 +0200

    Intial background subtraction from opencv

---
+++ blank_seq_removal/blank_seq_remove.py
@@ -0,0 +1,16 @@
+import cv2
+
+
+class BlankSeqDetector:
+    def __init__(self):
+        self.backSub = cv2.createBackgroundSubtractorMOG2(2, 100, detectShadows=False)
+
+    def process(self, frame_buffer):
+        blured = cv2.GaussianBlur(src=frame_buffer[0], ksize=(15, 15), sigmaX=0)
+        mask = self.backSub.apply(blured)
+        cv2.imshow("Foreground Mask", mask)
+
+        contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        cv2.drawContours(frame_buffer[0], contours, -1, (0, 255, 0), 3)
+
+        return False
--- blank_seq_removal/main.py
+++ blank_seq_removal/main.py
@@ -1,4 +1,51 @@
 import cv2
+import os
+import yaml
+
+from collections import deque
+
+from blank_seq_removal.blank_seq_remove import BlankSeqDetector
+
+cfg_path = open('cfg.yml')
+cfg = yaml.load(cfg_path, Loader=yaml.FullLoader)
+step = 20
+
+
+def main_loop():
+    if cfg['use_camera']:
+        cap = cv2.VideoCapture(0)
+    else:
+        if os.path.exists(cfg['video_path']):
+            cap = cv2.VideoCapture(cfg['video_path'])
+        else:
+            raise FileNotFoundError("Video file doesn't exist.")
+
+    bsd = BlankSeqDetector()
+    buffer = deque(maxlen=cfg['buffer_size'])
+    is_static = False
+
+    while True:
+        ret, frame = cap.read()
+        h, w, c = frame.shape
+
+        buffer.append(frame)
+
+        if len(buffer) == cfg['buffer_size']:
+            is_static = bsd.process(buffer)
+            buffer.pop()
+
+        if is_static:
+            for i in range(0, h*step, step):
+                cv2.line(frame, (i, 0), (i, w), (0, 0, 255), 2)
+
+        cv2.imshow('Frame', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
 
 if __name__ == "__main__":
-    print("Hello")
+    main_loop()
