39ffd23c3295f198a3884d4c37d6b4b4f7152c30
adrianB3 <balanescuadrian71@gmail.com>
2020-11-02 01:19:50 +0200

    blank seq working ver

--- blank_seq_removal/blank_seq_remove.py
+++ blank_seq_removal/blank_seq_remove.py
@@ -10,7 +10,10 @@ class BlankSeqDetector:
         mask = self.backSub.apply(blured)
         cv2.imshow("Foreground Mask", mask)
 
-        contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         cv2.drawContours(frame_buffer[0], contours, -1, (0, 255, 0), 3)
 
+        # nb = cv2.countNonZero(mask)
+        if len(contours) < 3:
+            return True
         return False
--- blank_seq_removal/main.py
+++ blank_seq_removal/main.py
@@ -9,6 +9,8 @@ from blank_seq_removal.blank_seq_remove import BlankSeqDetector
 cfg_path = open('cfg.yml')
 cfg = yaml.load(cfg_path, Loader=yaml.FullLoader)
 step = 20
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('output.avi', fourcc, 30.0, (1024, 768))
 
 
 def main_loop():
@@ -37,13 +39,16 @@ def main_loop():
         if is_static:
             for i in range(0, h*step, step):
                 cv2.line(frame, (i, 0), (i, w), (0, 0, 255), 2)
-
+        elif cfg['write_video']:
+            resized = cv2.resize(frame, (1024, 768), interpolation=cv2.INTER_AREA)
+            out.write(resized)
         cv2.imshow('Frame', frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
     cap.release()
+    out.release()
     cv2.destroyAllWindows()
 
 
